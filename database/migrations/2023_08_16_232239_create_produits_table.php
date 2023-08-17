e7c0d57a7084efde9d75df45ce275727662e0784
Pier-Luc Lemay <pi-el_gangsta@hotmail.com>
2023-08-16 21:39:00 -0400

    Ajout de certaines migrations.
    	new file:   database/migrations/2023_08_16_231923_create_commandes_table.php
    	new file:   database/migrations/2023_08_16_232217_create_commandes_lignes_table.php
    	new file:   database/migrations/2023_08_16_232239_create_produits_table.php
    	new file:   database/migrations/2023_08_16_232253_create_partenaires_table.php
    	new file:   database/migrations/2023_08_16_232310_create_categorie_produit_table.php
    	new file:   database/migrations/2023_08_16_232323_create_categorie_table.php

---
+++ database/migrations/2023_08_16_232239_create_produits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id('id', 11);
+            $table->string('nom', 100);
+            $table->text('description')->nullable();
+            $table->decimal('prix', 10, 2);
+            $table->integer('rabais', 11)->nullable();
+            $table->image('image', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
