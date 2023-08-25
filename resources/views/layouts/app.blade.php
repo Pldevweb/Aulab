ff2b142bff746f79f905d34e78ec6609b94ee03d
Pier-Luc Lemay <pi-el_gangsta@hotmail.com>
2023-08-24 20:02:41 -0400

    views update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="storage/css/styles.css">
     <title>AULAB</title>
 @extends('layouts.base')
 </head>
