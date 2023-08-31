221ffe6ac86c2b861f6c5e645b90bfc39e4efc7a
Pier-Luc Lemay <pi-el_gangsta@hotmail.com>
2023-08-31 18:53:49 -0400

    contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-
-  
+    <div class="container my-24 mx-auto md:px-6">
+        <section class="mb-32 text-center">
+            <div class="mx-auto max-w-[700px] md:px-3">
+                <h1 class="text-3xl text-[#5e5e4a] mb-16 uppercase">Contactez-nous</h1>
+                <form>
+                    <div class="relative mb-6">
+                        <input type="text"
+                            class="border border-gray-300 peer block min-h-[auto] w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none focus:placeholder:opacity-100 focus:text-primary"
+                            id="exampleInput90" placeholder="Nom" />
+                    </div>
+                    <div class="relative mb-6">
+                        <input type="email"
+                            class="border border-gray-300 peer block min-h-[auto] w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none focus:placeholder:opacity-100 focus:text-primary"
+                            id="exampleInput91" placeholder="Courriel" />
+                    </div>
+                    <div class="relative mb-6">
+                        <textarea
+                            class="border border-gray-300 peer block min-h-[auto] w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none focus:placeholder:opacity-100"
+                            id="exampleFormControlTextarea1" rows="3" placeholder="Message"></textarea>
+                    </div>
+                    <div class="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
+                        <input
+                            class="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                            type="checkbox" value="" id="exampleCheck96" checked />
+                        <label class="inline-block pl-[0.15rem] hover:cursor-pointer" for="exampleCheck96">
+                            Envoyez-moi une copie de ce message.
+                        </label>
+                    </div>
+                    <button type="button" data-te-ripple-init data-te-ripple-color="light"
+                        class="bg-[#5e5e4a] mb-6 inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] hover:bg-primary-600 focus:bg-primary-600 focus:outline-none focus:ring-0 active:bg-primary-700 dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                        Envoyez
+                    </button>
+                </form>
+            </div>
+        </section>
+    </div>
 @endsection
