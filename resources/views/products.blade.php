04183eb3ab0bd408d68174b357a44216041b3e96
Pier-Luc Lemay <pi-el_gangsta@hotmail.com>
2023-09-11 22:23:51 -0400

    image fix

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,11 +10,11 @@
                 </div>
             </a>
             @foreach ($categories as $categorie)
-            <a href="{{ route('productsbycategorie', $categorie->slug) }}" class="m-1 basis-1/4">
-                <div class="text-main-color w-full rounded-lg shadow-md p-2 mb-3 text-xs text-center notactive">
-                    {{ $categorie->name }}
-                </div>
-            </a>
+                <a href="{{ route('productsbycategorie', $categorie->slug) }}" class="m-1 basis-1/4">
+                    <div class="text-main-color w-full rounded-lg shadow-md p-2 mb-3 text-xs text-center notactive">
+                        {{ $categorie->name }}
+                    </div>
+                </a>
             @endforeach
         </div>
         <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
@@ -24,7 +24,9 @@
                     <p class="text-main-color">{{ $product->description }}</p>
                     <p class="text-lg mt-2 text-main-color font-bold">{{ $product->price }} $</p>
                     <p class="text-sm text-main-color">Rabais {{ $product->discount }}%</p>
-                    <img src="{{ $product->image }}" alt="{{ $product->name }}" class="mt-4 w-full h-48 object-cover">
+                    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"
+                        class="mt-4 w-full h-48 object-cover">
+
                 </div>
             @endforeach
         </div>
