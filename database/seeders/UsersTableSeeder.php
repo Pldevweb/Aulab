6eda4f5e0e6a6381e37d6727cf598be2ff9e735b
nine9 <119972612+Nine9Design@users.noreply.github.com>
2023-09-17 11:25:36 -0400

    Test nouvelle branche

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,31 +14,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
-
         \DB::table('users')->delete();
-
-        \DB::table('users')->insert(array (
-            0 =>
-            array (
-                'id' => 1,
-                'role_id' => 1,
-                'name' => 'Danyka',
-                'email' => 'danyka@hotmail.com',
-                'avatar' => 'users/default.png',
-                'email_verified_at' => NULL,
-                'password' => '$2y$10$HtgwsQkHPWzl2IMnfnWzVusT5xKdpBHHbJGat6QgsHUoFP643Gl.2',
-                'address' => NULL,
-                'city' => NULL,
-                'zipcode' => NULL,
-                'country' => NULL,
-                'remember_token' => NULL,
-                'settings' => NULL,
-                'created_at' => '2023-09-14 18:57:00',
-                'updated_at' => '2023-09-14 18:57:01',
-            ),
-        ));
-
-
     }
 }
